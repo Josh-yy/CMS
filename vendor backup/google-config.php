5f33704b2e0ce52dd67908717ca2979364dd20fe
Josh-yy <126300620+Josh-yy@users.noreply.github.com>
2025-08-10 13:28:04 +0800

    delete duplicate vendor file

--- vendor backup/google-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/autoload.php';
-
-$client = new Google_Client();
-$client->setClientId('393155941759-mh07qkfub8q6b1ghpbjnm7snlm9htibu.apps.googleusercontent.com'); // Replace with your Client ID
-$client->setClientSecret('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'); // Replace with your Client Secret
-$client->setRedirectUri('http://localhost:8000/login'); // Replace with your redirect URI
-$client->addScope('email');
-$client->addScope('profile');
